bf8e5b21e53af1a6fc973f9c07fc04206c12bbc3
xujn@tuya.com <xujn@tuya.com>
2024-05-19 23:52:34 +0800

    update config

--- dot_config/i3/powermenu.sh
+++ dot_config/i3/powermenu.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-ans=$(zenity --info --title "PowerMenu" --text "Choose an action" --ok-label lock --extra-button suspend --extra-button poweroff --extra-button reboot --extra-button expand --extra-button mirror --extra-button single)
+ans=$(zenity --info --title "PowerMenu" --text "Choose an action" --ok-label suspend --extra-button expand --extra-button single --extra-button mirror --extra-button poweroff --extra-button reboot)
 rc=$?
 case "$rc-$ans" in
 	"0-")
-		echo "lock"
-		i3lock -i ~/.config/i3/lock.png -t
-		;;
-	"1-suspend")
 		echo "suspend"
 		systemctl suspend
+		;;
+	"1-expand")
+		xrandr --output HDMI-1 --auto --left-of eDP-1
+		;;
+	"1-single")
+		xrandr --output HDMI-1 --off
+		;;
+	"1-mirror")
+		xrandr --output HDMI-1 --same-as eDP-1
 		;;
 	"1-poweroff")
 		echo "poweroff"
@@ -17,15 +22,6 @@ case "$rc-$ans" in
 	"1-reboot")
 		echo "reboot"
 		systemctl reboot
-		;;
-	"1-expand")
-		xrandr --output HDMI-1 --auto --left-of eDP-1
-		;;
-	"1-mirror")
-		xrandr --output HDMI-1 --same-as eDP-1
-		;;
-	"1-single")
-		xrandr --output HDMI-1 --off
 		;;
 esac
 
