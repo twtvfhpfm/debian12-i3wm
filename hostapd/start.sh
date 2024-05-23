9a3505e136793957b82a2143b0089765a01fc1b7
xu jiannan <xu_jiannan@126.com>
2024-05-23 22:30:48 +0800

    add hostapd config

--- hostapd/start.sh
+++ hostapd/start.sh
@@ -7,6 +7,7 @@ killall dnsmasq
 sleep 1
 /sbin/hostapd -B hostapd.conf
 /sbin/dnsmasq -C dnsmasq.conf
+ip addr replace 192.168.233.1/24 dev wlan-xxxxxx 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-/sbin/iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
+/sbin/iptables -t nat -A POSTROUTING -o eth-xxxxx -j MASQUERADE
 
